8a55def2f58122d088bff9960b9cbafce65d0e14
gibs0nnn <gibs0n@knights.ucf.edu>
2021-09-13 19:14:30 -0400

    Cleanliness and Comments

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,16 +12,24 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
+        //Input
         System.out.print("What is the length of the room in feet? ");
-        int length = scanner.nextInt();
+        String strLength = scanner.next();
         System.out.print("What is the width of the room in feet? ");
-        int width = scanner.nextInt();
+        String strWidth = scanner.next();
 
+        //Parsing
+        int length = Integer.parseInt(strLength);
+        int width = Integer.parseInt(strWidth);
+
+        //Calculations
         int areaFeet = length * width;
         double areaMeters = (double) areaFeet * 0.09290304;
 
+        //Output
+        System.out.println("You entered dimensions of " + length + " feet by " + width + " feet.");
         System.out.println("The area is");
         System.out.println(areaFeet + " square feet");
-        System.out.println(areaMeters + " square meters");
+        System.out.println(Math.round(areaMeters * 1000.0) / 1000.0 + " square meters");
     }
 }
